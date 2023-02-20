90defd6fbaa2e7a47835560861e837cfa7e7bb3b
f-mehdibayli <farid.mehdibayli@gmail.com>
2023-02-20 22:36:07 +0400

    new

---
+++ javascript.js
@@ -0,0 +1,10 @@
+
+
+const currencyBoxes=document.querySelector('.one-box')
+
+// console.log(currencyBoxes)
+
+currencyBoxes.addEventListener('click',(item)=>{
+    console.log(item)
+    item.style.backgroundColor='red' ;
+})
