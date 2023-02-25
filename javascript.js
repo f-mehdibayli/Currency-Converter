42cd351bed1ece92f16f83bb1ac7b24d1563ccbf
Farid <farid.mehdibayli@gmail.com>
2023-02-25 15:23:50 +0400

    Endd Server Project

--- javascript.js
+++ javascript.js
@@ -7,11 +7,35 @@ const outputValue=document.querySelector('.output-value input')
 const infoValue=document.querySelector('.input-value .currency-value')
 const infoValue2=document.querySelector('.output-value .currency-value')
 
-let abc;
-const currencyGet=async(input,output)=>{
-    const response=await fetch(`https://api.exchangerate.host/latest?base=${input}&symbols=${output}`)
-    const data= response.json();
-    data.then(data=> {
+
+const dataValue=async (input,output)=>{
+    try {
+        const response=await fetch(`https://api.exchangerate.host/latest?base=${input}&symbols=${output}`)
+        const data=await response.json();
+        return data
+            
+    } catch (error) {
+        alert('Internetle əlaqədar problem cixdi')
+        
+    }
+    
+}
+
+function formatNum(num){
+    const strNum=num.toString();
+    const parts=strNum.split('.')
+    if(parts.length === 1){
+        return num
+    }else if(parts[1].length <=4 ){
+        return num
+    }else{
+        return parseFloat(num.toFixed(4))
+    }
+}
+
+
+const currencyGet=(input,output)=>{
+    dataValue(input,output).then(data=> {
        
         const baseCurrency=input
         const targetCurrency=output 
@@ -20,39 +44,55 @@ const currencyGet=async(input,output)=>{
             outputValue.value=''
         }else{
             const end=inputValue.value*resultValue
-        outputValue.value=end
+            const endNumber=formatNum(end)
+            outputValue.value=endNumber
         }
-        
-
         const info=`1 ${baseCurrency} = ${resultValue} ${targetCurrency}`
         infoValue.innerHTML=info
         
-        const info1=`1 ${targetCurrency} = ${resultValue} ${baseCurrency}`
+    }).catch(err=>console.log('Error:',err))
+
+
+    dataValue(output,input).then(data=> {
+        const baseCurrency=output
+        const targetCurrency=input 
+        const resultValue=data.rates[targetCurrency]
+        const info1=`1 ${baseCurrency} = ${resultValue} ${targetCurrency}`
         infoValue2.innerHTML=info1
+       
     }).catch(err=>console.log('Error:',err))
   }
 
 
+
   const currencyGet1=async(input,output)=>{
-    const response=await fetch(`https://api.exchangerate.host/latest?base=${input}&symbols=${output}`)
-    const data=response.json();
-    data.then(data=> {
+    dataValue(input,output).then(data=> {
 
         const baseCurrency=input
         const targetCurrency=output
         const resultValue=data.rates[targetCurrency]
-        if(outputValue.value?.trim() ===''){
+        if(outputValue.value ===''){
             inputValue.value=''
         }else{ 
             const end=outputValue.value*resultValue
-            inputValue.value=end
+            console.log(end)
+            const endNumber=formatNum(end)
+            inputValue.value=endNumber
          }
        
         const info=`1 ${baseCurrency} = ${resultValue} ${targetCurrency}`
         infoValue2.innerHTML=info
+
+        dataValue(output,input).then(data=> {
+            const baseCurrency=output
+            const targetCurrency=input 
+            const resultValue=data.rates[targetCurrency]
+            const info1=`1 ${baseCurrency} = ${resultValue} ${targetCurrency}`
+            infoValue.innerHTML=info1
+           
+        }).catch(err=>console.log('Error:',err))
          
-        const info1=`1 ${targetCurrency} = ${resultValue} ${baseCurrency}`
-        infoValue.innerHTML=info1
+        
     }).catch(err=>console.log('Error:',err))
   }
  
@@ -67,14 +107,19 @@ const currencyGet=async(input,output)=>{
         outputBox.forEach(item=>{
             if(item.classList.contains('currency-box-active')){
                 let data2=item.innerHTML
-            currencyGet(data1,data2)
-
-
+                if(data1===data2){
+                    outputValue.value=inputValue.value
+                    infoValue.innerHTML=`1 ${data1} = 1 ${data2}`
+                    infoValue2.innerHTML=`1 ${data1} = 1 ${data2}`
+                }else{
+                    currencyGet(data1,data2)
+                }
         }
     }
     )}
     })}
     currencyValue()
+
     
     function currencyValue1(){
         outputBox.forEach(item=>{
@@ -84,17 +129,19 @@ const currencyGet=async(input,output)=>{
             inputBox.forEach(item=>{
                 if(item.classList.contains('currency-box-active')){
                     let data1=item.innerHTML
-                currencyGet1(data2,data1)
-    
+                    if(data2===data1){
+                        inputValue.value=outputValue.value
+                        infoValue2.innerHTML=`1 ${data2} = 1 ${data1}`
+                        infoValue.innerHTML=`1 ${data2} = 1 ${data1}`
+                    }else{
+                        currencyGet1(data2,data1)
+                    }
             }
         }
         )}
         })}
 
-    
-        
- 
-    
+
     
     inputBox.forEach(element => {
         element.addEventListener('click', e => {
@@ -109,7 +156,7 @@ const currencyGet=async(input,output)=>{
           element.addEventListener('click', e => {
               outputBox.forEach(el => el.classList.remove('currency-box-active'));
               e.target.classList.add('currency-box-active'); 
-              currencyValue1()   
+              currencyValue()   
           });
         });
 
@@ -120,7 +167,7 @@ const currencyGet=async(input,output)=>{
               if(this.value[0] === '.'){
                 this.value=this.value.replace(/[^0-9]/g, "")
               }
-              if(this.value.split(".").length -1 > 1){
+              if(this.value.split(".").length - 1 > 1){
                 this.value = this.value.slice(0, -1);
               }
             });
@@ -138,5 +185,9 @@ const currencyGet=async(input,output)=>{
             inputNumber(outputValue)
             currencyValue1()
         })
-        console.log('abc:',abc)
+       
         
+
+
+    
+   
